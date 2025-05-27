5ea8827c64059c8949b8353a5a4e6bed5e8f3e72
kajal mishra <111572290+mishrakajal2200@users.noreply.github.com>
2025-05-26 21:18:48 -0500

    changed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -130,7 +130,7 @@ const handleLogin = async (e) => {
   e.preventDefault();
 
   try {
-    const response = await fetch("http://localhost:5000/api/auth/admin/login", {
+    const response = await fetch("https://gsi-backend-1.onrender.com/api/auth/admin/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
