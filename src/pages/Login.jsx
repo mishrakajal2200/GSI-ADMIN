367f5a07d3fb40c2388aec4c4b23f1a68bd533c9
kajal mishra <111572290+mishrakajal2200@users.noreply.github.com>
2025-06-04 10:56:52 -0500

    kajal

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,7 +17,7 @@ const handleLogin = async (e) => {
 
   try {
     const { data } = await axios.post(
-      "https://gsi-backend-1.onrender.com/api/auth/admin/login",
+      "https://gsi-backend-1.onrender.com/api/admin/login",
       {
         email,
         password,
